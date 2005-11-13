5d4e96d389f6e68f6b88589d45185ae5914ca798
jamesu <jamesu@gmail.com>
2005-11-13 21:34:12 +0000

    - Removed getPath and blenderRun scripts, since they are no longer needed. - Added install scripts for windows, mac, and linux.

--- torqueplugin/util/blenderRun.bat
+++
@@ -1,4 +0,0 @@
-rem Add the directory of the Python Scripts to the PYTHONPATH Var
-rem Launch blender (add full path)
-set PYTHONPATH=%PYTHONPATH%;c:\Torque_Exporter
-"C:\Program Files\Blender Foundation\Blender\blender.exe" -windowed
