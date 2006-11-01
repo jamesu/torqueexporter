75400b13e0bb01059af19484aa219fbf978d35c1
jsgreenawalt <jsgreenawalt@gmail.com>
2006-11-01 22:49:13 +0000

    Fixed license header (had random code snippets inserted somehow)

--- torqueplugin/DtsShape_Blender.py
+++ torqueplugin/DtsShape_Blender.py
@@ -3,7 +3,7 @@ Dts.Shape_Blender.py
 
 Copyright (c) 2005 - 2006 James Urquhart(j_urquhart@btinternet.com)
 
-Permission is hereby granted, free of charge, to any person obtainingTorque_Util.dump_write
+Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
 "Software"), to deal in the Software without restriction, including
 without limitation the rights to use, copy, modify, merge, publish,
@@ -12,7 +12,7 @@ permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
 
 The above copyright notice and this permission notice shall be
-included in all copies or substantial poTorque_Util.dump_writelnrtions of the Software.
+included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
