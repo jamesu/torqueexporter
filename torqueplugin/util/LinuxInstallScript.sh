d0abe63a5c0f5eb3a0dd3bab1854062ffb2f421b
jsgreenawalt <jsgreenawalt@gmail.com>
2008-03-12 00:07:05 +0000

    Renamed Linux Install Script.

---
+++ torqueplugin/util/LinuxInstallScript.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Verify scripts folder
+if [ -e ~/.blender ] 
+then
+	if [ -e ~/.blender/scripts ]
+	then
+		echo "Install: Scripts folder ready."
+	else
+		if ( mkdir ~/.blender/scripts )
+		then
+			echo "Install: Scripts folder created."
+		else
+			echo "Install: Scripts folder could not be created!"
+			exit 1
+		fi
+	fi
+else
+	if ( ( mkdir ~/.blender ) && ( mkdir ~/.blender/scripts ) )
+	then
+		echo "Install: All folders created."
+	else
+		echo "Install: Script folders could not be created!"
+		exit 1
+	fi
+fi
+
+# Now we can copy the files over
+if ( [ -e Dts_Blender.py ] && [ -e DTSPython ] )
+then
+	if ( cp -Rfv *.py DTSPython ~/.blender/scripts/ )
+	then
+		echo "Install: Scripts successfully copied."
+	else
+		echo "Install: Scripts could not be copied."
+		exit 1
+	fi
+else
+	echo "Install: Selected folder does not contain exporter scripts!"
+	exit 1
+fi
+exit 0
+
