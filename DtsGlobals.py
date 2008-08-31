4d6380351570f84a07c864a52665084eaac43352
jsgreenawalt <jsgreenawalt@gmail.com>
2008-08-31 00:02:55 +0000

    Fixed filename in copyright header.

--- DtsGlobals.py
+++ DtsGlobals.py
@@ -1,7 +1,7 @@
 
 '''
-Dts_Blender.py
-Copyright (c) 2003 - 2006 James Urquhart(j_urquhart@btinternet.com)
+DtsGlobals.py
+Copyright (c) 2003 - 2008 James Urquhart(j_urquhart@btinternet.com)
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
