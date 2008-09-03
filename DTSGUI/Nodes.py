3ad0bebbf681beb3cec89591b5d0dcabdbac2bba
jsgreenawalt <jsgreenawalt@gmail.com>
2008-09-03 16:15:00 +0000

    Rewrote redundant code, added match by type controls, and controls to select the number of items to display on each line of the nodes list (defaults to 5).

--- DTSGUI/Nodes.py
+++ DTSGUI/Nodes.py
@@ -39,29 +39,52 @@ class NodeControlsClass:
 	def __init__(self, guiNodeListSubtab):
 		global globalEvents
 
+		# number of items per line in the node list
+		self.itemsPerLine = 5
+		
 		# initialize GUI controls
-		self.guiNodeText = Common_Gui.SimpleText("guiNodeText", "Nodes that should be exported :", None, self.resize)
+		self.guiNodeListLabel = Common_Gui.SimpleText("guiNodeListLabel", "Nodes that should be exported :", None, self.resize)
 		self.guiNodeList = Common_Gui.BoneListContainer("guiNodeList", None, None, self.resize)
-		self.guiMatchText =  Common_Gui.SimpleText("guiMatchText", "Match pattern", None, self.resize)
+		self.guiMatchLabel =  Common_Gui.SimpleText("guiMatchLabel", "Match pattern", None, self.resize)
 		self.guiPatternText = Common_Gui.TextBox("guiPatternText", "", "pattern to match bone names, asterix is wildcard", 6, self.handleEvent, self.resize)
 		self.guiPatternOn = Common_Gui.BasicButton("guiPatternOn", "On", "Turn on export of bones matching pattern", 7, self.handleEvent, self.resize)
 		self.guiPatternOff = Common_Gui.BasicButton("guiPatternOff", "Off", "Turn off export of bones matching pattern", 8, self.handleEvent, self.resize)
-		self.guiRefresh = Common_Gui.BasicButton("guiRefresh", "Refresh", "Refresh bones list", 9, self.handleEvent, self.resize)
+		self.guiNumItemsLabel = Common_Gui.SimpleText("guiNumItemsLabel", "List Display", None, self.resize)
+		self.guiNumItemsSlider = Common_Gui.NumberPicker("guiNumItemsSlider", "Items per line:", "Number of items per line to display in the node list", 9, self.handleEvent, self.resize)
+		self.guiMatchTypeLabel = Common_Gui.SimpleText("guiMatchTypeLabel", "Match by type", None, self.resize)
+		self.guiMatchTypePulldown = Common_Gui.ComboBox("guiMatchTypePulldown", "Node Type", "Type of node to toggle", 10, self.handleEvent, self.resize)
+		self.guiTypeOn = Common_Gui.BasicButton("guiTypeOn", "On", "Turn on export of nodes matching type", 11, self.handleEvent, self.resize)
+		self.guiTypeOff = Common_Gui.BasicButton("guiTypeOff", "Off", "Turn off export of nodes matching pattern", 12, self.handleEvent, self.resize)
+
+		self.guiRefresh = Common_Gui.BasicButton("guiRefresh", "Refresh", "Refresh bones list", 15, self.handleEvent, self.resize)
 				
 		# set initial states
 		self.guiPatternText.value = "*"
+		self.guiNumItemsSlider.value = 5
+		self.guiNumItemsSlider.min = 1
+		self.guiNumItemsSlider.max = 40
+		self.guiMatchTypePulldown.items.append("Object Nodes")
+		self.guiMatchTypePulldown.items.append("Bone Nodes")
+		self.guiMatchTypePulldown.selectStringItem("Object Nodes")
 		
 		# add controls to containers
-		guiNodeListSubtab.addControl(self.guiNodeText)
+		guiNodeListSubtab.addControl(self.guiNodeListLabel)
 		guiNodeListSubtab.addControl(self.guiNodeList)
-		guiNodeListSubtab.addControl(self.guiMatchText)
+		guiNodeListSubtab.addControl(self.guiMatchLabel)
 		guiNodeListSubtab.addControl(self.guiPatternText)
 		guiNodeListSubtab.addControl(self.guiPatternOn)
 		guiNodeListSubtab.addControl(self.guiPatternOff)
+		guiNodeListSubtab.addControl(self.guiNumItemsLabel)
+		guiNodeListSubtab.addControl(self.guiNumItemsSlider)
+		guiNodeListSubtab.addControl(self.guiMatchTypeLabel)
+		guiNodeListSubtab.addControl(self.guiMatchTypePulldown)
+		guiNodeListSubtab.addControl(self.guiTypeOn)
+		guiNodeListSubtab.addControl(self.guiTypeOff)
 		guiNodeListSubtab.addControl(self.guiRefresh)
 		
-		# populate bone grid
+		# populate node grid
 		self.populateNodeGrid()
+		
 		
 	def cleanup(self):
 		'''
@@ -71,12 +94,18 @@ class NodeControlsClass:
 		Note: __del__ is not guaranteed to be called for objects that still
 		exist when the interpreter exits.
 		'''
-		del self.guiNodeText
+		del self.guiNodeListLabel
 		del self.guiNodeList
-		del self.guiMatchText
+		del self.guiMatchLabel
 		del self.guiPatternText
 		del self.guiPatternOn
 		del self.guiPatternOff
+		del self.guiNumItemsLabel
+		del self.guiNumItemsSlider
+		del self.guiMatchTypeLabel
+		del self.guiMatchTypePulldown
+		del self.guiTypeOn
+		del self.guiTypeOff
 		del self.guiRefresh
 
 
@@ -106,38 +135,79 @@ class NodeControlsClass:
 								del Prefs['BannedNodes'][i]
 					elif control.name == "guiPatternOff":
 						Prefs['BannedNodes'].append(name)
-			self.clearBoneGrid()
 			self.populateNodeGrid()
 		elif control.name == "guiRefresh":
-			self.clearBoneGrid()
 			self.populateNodeGrid()
-
+		elif control.name == "guiNumItemsSlider":
+			self.itemsPerLine = control.value
+			self.populateNodeGrid()
+		elif control.name == "guiTypeOn":
+			typeStr = self.guiMatchTypePulldown.getSelectedItemString()
+			if typeStr == "Object Nodes":
+				nodeList = SceneInfo.getObjectNodeNames()
+			else: nodeList = SceneInfo.getBoneNodeNames()			
+			for name in nodeList:
+				for i in range(len(Prefs['BannedNodes'])-1, -1, -1):
+					boneName = Prefs['BannedNodes'][i].upper()
+					if name.upper() == boneName:
+						del Prefs['BannedNodes'][i]
+						print "removing node:", name
+			self.populateNodeGrid()
+		elif control.name == "guiTypeOff":
+			typeStr = self.guiMatchTypePulldown.getSelectedItemString()
+			if typeStr == "Object Nodes":
+				nodeList = SceneInfo.getObjectNodeNames()
+			else: nodeList = SceneInfo.getBoneNodeNames()			
+			for name in nodeList:
+				Prefs['BannedNodes'].append(name.upper())
+				print "adding node:", name
+			self.populateNodeGrid()
+		
 	def resize(self, control, newwidth, newheight):
-		if control.name == "guiNodeText":
+		print "ordinary resize callback called..."
+		print " control.name =", control.name
+		if control.name == "guiNodeListLabel":
 			control.x, control.y = 10,newheight-20
 		elif control.name == "guiNodeList":
 			#control.x, control.y, control.width, control.height = 10,70, 470,242
 			control.x, control.y = 10, 90
 			control.width, control.height = newwidth - 20, newheight-120
-		elif control.name == "guiMatchText":
+		elif control.name == "guiMatchLabel":
 			control.x, control.y = 10,newheight-285
 		elif control.name == "guiPatternText":
-			control.x, control.y, control.width = 10,newheight-315, 70
+			control.x, control.y, control.width = 95,newheight-290, 100
 		elif control.name == "guiPatternOn":
-			control.x, control.y, control.width = 84,newheight-315, 35
+			control.x, control.y, control.width = 199,newheight-290, 35
 		elif control.name == "guiPatternOff":
-			control.x, control.y, control.width = 121,newheight-315, 35
-		elif control.name == "guiRefresh":
-			control.x, control.y, control.width = 400,newheight-315, 75
+			control.x, control.y, control.width = 236,newheight-290, 35
+		elif control.name == "guiMatchTypeLabel":
+			control.x, control.y, control.width = 10,newheight-315, 150
+		elif control.name == "guiMatchTypePulldown":
+			control.x, control.y, control.width = 95,newheight-320, 100
+		elif control.name == "guiTypeOn":
+			control.x, control.y, control.width = 199,newheight-320, 35
+		elif control.name == "guiTypeOff":
+			control.x, control.y, control.width = 236,newheight-320, 35
+		elif control.name == "guiNumItemsLabel":
+			control.x, control.y, control.width = 355,newheight-285, 150
+		elif control.name == "guiNumItemsSlider":
+			control.x, control.y, control.width = 355,newheight-315, 125
 
+		elif control.name == "guiRefresh":
+			control.x, control.y, control.width = 400,newheight-25, 75
+		
+	def resizeListButton(self, control, newwidth, newheight):
+		listWidth = self.guiNodeList.width
+		buttonWidth = (listWidth-self.guiNodeList.barWidth)/self.itemsPerLine
+		buttonHeight = self.guiNodeList.childHeight
+		control.x, control.y = 1 + (control.columnNum*(buttonWidth)), 0
+		control.width, control.height = buttonWidth, buttonHeight
+
+	#def resizeListLine(self, control, newwidth, newheight):
+	#	self.newwidth = 
+	
 	def guiNodeListItemCallback(self, control):
-		global guiSeqActList
 		Prefs = DtsGlobals.Prefs
-		# Determine id of clicked button
-		if control.evt == 40:
-			calcIdx = 0
-		else:
-			calcIdx = (control.evt - 40) #/ 4
 		real_name = control.text.upper()
 		if control.state:
 			# Remove entry from BannedNodes
@@ -148,75 +218,43 @@ class NodeControlsClass:
 		else:
 			Prefs['BannedNodes'].append(real_name)
 
-	def createBoneListitem(self, bone1, bone2, bone3, bone4, bone5, startEvent):
-		#seqPrefs = getSequenceKey(seq_name)
-		# Note on positions:
-		# It quicker to assign these here, as there is no realistic chance scaling being required.
+	# create a row of node buttons
+	def createBoneListitem(self, boneNames, startEvent):
+		if len(boneNames) == 0: return # don't even bother
+		Prefs = DtsGlobals.Prefs
 		guiContainer = Common_Gui.BasicContainer("", None, None)
 		guiContainer.fade_mode = 0
 		guiContainer.borderColor = None
-		if bone1 != None:
-			guiBone1 = Common_Gui.ToggleButton("guiBone_" + bone1, bone1, "Toggle Status of " + bone1, startEvent, self.guiNodeListItemCallback, None)
-			guiBone1.x, guiBone1.y = 1, 0
-			guiBone1.width, guiBone1.height = 90, 19
-			guiBone1.state = True
-			guiContainer.addControl(guiBone1)
-		if bone2 != None:
-			guiBone2 = Common_Gui.ToggleButton("guiBone_" + bone2, bone2, "Toggle Status of " + bone2, startEvent+1, self.guiNodeListItemCallback, None)
-			guiBone2.x, guiBone2.y = 92, 0
-			guiBone2.width, guiBone2.height = 90, 19
-			guiBone2.state = True
-			guiContainer.addControl(guiBone2)
-		if bone3 != None:
-			guiBone3 = Common_Gui.ToggleButton("guiBone_" + bone3, bone3, "Toggle Status of " + bone3, startEvent+3, self.guiNodeListItemCallback, None)
-			guiBone3.x, guiBone3.y = 183, 0
-			guiBone3.width, guiBone3.height = 90, 19
-			guiBone3.state = True
-			guiContainer.addControl(guiBone3)
-		if bone4 != None:
-			guiBone4 = Common_Gui.ToggleButton("guiBone_" + bone4, bone4, "Toggle Status of " + bone4, startEvent+4, self.guiNodeListItemCallback, None)
-			guiBone4.x, guiBone4.y = 274, 0
-			guiBone4.width, guiBone4.height = 89, 19
-			guiBone4.state = True
-			guiContainer.addControl(guiBone4)	
-		if bone5 != None:
-			guiBone5 = Common_Gui.ToggleButton("guiBone_" + bone5, bone5, "Toggle Status of " + bone5, startEvent+5, self.guiNodeListItemCallback, None)
-			guiBone5.x, guiBone5.y = 364, 0
-			guiBone5.width, guiBone5.height = 89, 19
-			guiBone5.state = True
-			guiContainer.addControl(guiBone5)
+		guiBones = []
+		for i in range(0, len(boneNames)):
+			boneName = boneNames[i]
+			if boneName != None:
+				guiBones.append(Common_Gui.ToggleButton("guiBone_" + boneName, boneName, "Toggle Status of " + boneName, startEvent+i, self.guiNodeListItemCallback, self.resizeListButton))
+				guiBones[i].state = not (boneName.upper() in Prefs['BannedNodes'])
+				guiBones[i].columnNum = i
+				guiContainer.addControl(guiBones[i])
+
+		
 		return guiContainer
+		
 
 	def populateNodeGrid(self):
-		global export_tree, guiNodeList
+		self.clearBoneGrid()
 		SceneInfo = DtsGlobals.SceneInfo
-		Prefs = DtsGlobals.Prefs
 		evtNo = 40
-		count = 0
 		names = []
+		# add one row at a time
 		for name in SceneInfo.getAllNodeNames():
 			names.append(name)
-			if len(names) == 5:
-				self.guiNodeList.addControl(self.createBoneListitem(names[0],names[1],names[2],names[3],names[4], evtNo))
-				self.guiNodeList.controls[count].controls[0].state = not (self.guiNodeList.controls[count].controls[0].text.upper() in Prefs['BannedNodes'])
-				self.guiNodeList.controls[count].controls[1].state = not (self.guiNodeList.controls[count].controls[1].text.upper() in Prefs['BannedNodes'])
-				self.guiNodeList.controls[count].controls[2].state = not (self.guiNodeList.controls[count].controls[2].text.upper() in Prefs['BannedNodes'])
-				self.guiNodeList.controls[count].controls[3].state = not (self.guiNodeList.controls[count].controls[3].text.upper() in Prefs['BannedNodes'])
-				self.guiNodeList.controls[count].controls[4].state = not (self.guiNodeList.controls[count].controls[4].text.upper() in Prefs['BannedNodes'])
-
-				evtNo += 6
-				count += 1
+			if len(names) == self.itemsPerLine:
+				self.guiNodeList.addControl(self.createBoneListitem(names, evtNo))
+				evtNo += self.itemsPerLine
 				names = []
 		# add leftovers in last row
 		if len(names) > 0:
-			for i in range(len(names)-1, 5):
+			for i in range(len(names)-1, self.itemsPerLine):
 				names.append(None)
-			self.guiNodeList.addControl(self.createBoneListitem(names[0],names[1],names[2],names[3], names[4], evtNo))
-			if names[0] != None: self.guiNodeList.controls[count].controls[0].state = not (self.guiNodeList.controls[count].controls[0].text.upper() in Prefs['BannedNodes'])
-			if names[1] != None: self.guiNodeList.controls[count].controls[1].state = not (self.guiNodeList.controls[count].controls[1].text.upper() in Prefs['BannedNodes'])
-			if names[2] != None: self.guiNodeList.controls[count].controls[2].state = not (self.guiNodeList.controls[count].controls[2].text.upper() in Prefs['BannedNodes'])
-			if names[3] != None: self.guiNodeList.controls[count].controls[3].state = not (self.guiNodeList.controls[count].controls[3].text.upper() in Prefs['BannedNodes'])
-			if names[4] != None: self.guiNodeList.controls[count].controls[4].state = not (self.guiNodeList.controls[count].controls[4].text.upper() in Prefs['BannedNodes'])
+			self.guiNodeList.addControl(self.createBoneListitem(names, evtNo))
 
 
 	def clearBoneGrid(self):
